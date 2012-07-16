b7cf778984b0040739428fd8a42ad3f0122bcaf0
nacamura <t.nacamura@gmail.com>
2012-07-16 16:26:56 +0900

    add ComicStrategy

--- config/initializers/constants_library_names.rb
+++ config/initializers/constants_library_names.rb
@@ -1,3 +1,4 @@
 # -*- encoding: utf-8 -*-
-STRATEGY_MAPPING = {"西東京市" => NishitokyoStrategy}.freeze
+STRATEGY_MAPPING = {"西東京市" => NishitokyoStrategy,
+                    "コミック" => ComicStrategy}.freeze
 LIBRARY_NAMES = STRATEGY_MAPPING.keys.freeze
