4dd2774667fd6bfe8a4958f9c6f46d0010241f31
nacamura <t.nacamura@gmail.com>
2012-07-16 16:25:40 +0900

    add new column to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120708070008) do
+ActiveRecord::Schema.define(:version => 20120716044648) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120708070008) do
     t.integer  "library_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "release"
   end
 
   add_index "books", ["library_id"], :name => "index_books_on_library_id"
