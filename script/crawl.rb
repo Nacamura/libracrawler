51dd7ab288a753cd6469a52cf0d274c333c53fcc
nacamura <t.nacamura@gmail.com>
2012-09-30 23:01:53 +0900

    modify kickscript for alart

--- script/crawl.rb
+++ script/crawl.rb
@@ -1,8 +1,12 @@
 Rails.logger.info "Start #{start=Time.now}"
 
-Library.includes(:books).all.each do |lib|
-  lib.sendmail = true
-  lib.check_new_book
+begin
+  Library.includes(:books).all.each do |lib|
+    lib.sendmail = true
+    lib.check_new_book
+  end
+rescue => ex
+  AlartMailer.alart_mail(ex.message).deliver
 end
 
 Rails.logger.info "Stop #{stop=Time.now}"
