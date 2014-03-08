d945059ee605c04ede83bab6f2f5653fddfb21ff
Nacamura <t.nacamura@gmail.com>
2014-03-08 18:14:17 +0900

    new version for Nishitokyo website renewal

--- app/helpers/nishitokyo_strategy.rb
+++ app/helpers/nishitokyo_strategy.rb
@@ -2,41 +2,42 @@ module NishitokyoStrategy
   include LogInterceptor
 
   def crawl
-    parse_to_books(get_html)
+    construct_books( make_book_strings(get_mechanize_res) )
   end
 
-  def get_html
-    RestClient.post('http://www.library.city.nishitokyo.lg.jp/cgi-bin/search',
-                    :referer=>"http://www.library.city.nishitokyo.lg.jp/search.shtml",
-                    "ITEM1"=>"AB", "KEY1"=>title.to_s.encode('Shift_JIS'), "COMP1"=>"3",
-                    "ITEM2"=>"CD", "KEY2"=>author.to_s.encode('Shift_JIS'), "COMP2"=>"3",
-                    "ITEM3"=>"EF", "KEY3"=>publisher.to_s.encode('Shift_JIS'), "COMP3"=>"3",
-                    "COND"=>"1", "SORT"=>"1", "YEARFROM"=>year.to_s.encode('Shift_JIS'),
-                    "YEARTO"=>year.to_s.encode('Shift_JIS'),
-                    "MAXVIEW"=>"300").encode('UTF-8', 'Shift_JIS',
-                    :invalid => :replace, :undef => :replace)
-  end
-
-  def parse_to_books(html)
-    crawled_books = []
-    doc = Hpricot html
-    (doc/'tr').each do |tr|
-      if( (tr/'td').inner_html=='' )
-        next
-      end
-      book_attr = []
-      (tr/'td').each_with_index do |td, index|
-        if(index < 2)
-          next
-        end
-        a_content = (td/'a').inner_html
-        book_attr << ( (a_content=='') ? td.inner_html : a_content )
-      end
-      b = Book.new(:title=>book_attr[0],:author=>book_attr[1],
-                   :publisher=>book_attr[2],:year=>book_attr[3])
-      crawled_books << b
+  def get_mechanize_res
+    agent = Mechanize.new
+    agent.user_agent_alias = 'Mac Safari'
+    search_page = agent.get 'http://www.library.city.nishitokyo.lg.jp/detailsearch?1'
+    form = search_page.form
+    if(year.blank?)
+      year = Time.now.year
+    elsif
+      year = year.to_i
     end
-    crawled_books
+    form.field_with(:name=>'textKey1').value = title.to_s
+    form.field_with(:name=>'textKey2').value = author.to_s
+    form.field_with(:name=>'textKey3').value = publisher.to_s
+    form.field_with(:name=>'textYearFrom').value = (year - 1).to_s
+    form.field_with(:name=>'textYearTo').value = (year + 1).to_s
+    form.field_with(:name=>'selectMaxView').value = "100"
+    form.click_button
+  end
+
+  def make_book_strings(mechanize_res)
+    book_strings = []
+    mechanize_res.search("tr").each do |tr|
+      tr.search("img").each do |img|
+        book_strings << img.attribute("alt").value
+      end
+    end
+    book_strings
+  end
+
+  def construct_books(book_strings)
+    book_strings.map do |str|
+      Book.new(:title=>str)
+    end
   end
 
 end
