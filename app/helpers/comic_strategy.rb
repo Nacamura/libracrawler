d257ff384c951af8dbe01ff94449488f751287bd
Nacamura <t.nacamura@gmail.com>
2014-03-14 23:59:00 +0900

    Change parser to Mechanize

--- app/helpers/comic_strategy.rb
+++ app/helpers/comic_strategy.rb
@@ -4,54 +4,44 @@ module ComicStrategy
   @@books_cache
 
   def crawl
-    @@books_cache ||= parse_to_books(get_joint_html)
+    @@books_cache ||= make_to_books(get_mechanize_res)
     filter_books(@@books_cache)
-  end
-
-  def get_joint_html
-    get_urls.inject('') do |html, url|
-      html + get_html(url)
-    end
-  end
-
-  def get_html(url)
-    begin
-      RestClient.get(url)
-    rescue RestClient::ResourceNotFound
-      'Resource for ' + url + ' is not found'
-    end
   end
 
   def get_urls
     urls = []
-    get_months.each do |m|
+    months = (0..2).map{|i| Time.now.months_since(i).strftime('%y%m')}
+    months.each do |m|
       urls << 'http://www.taiyosha.co.jp/comic/comic' + m + '_date1.html'
       urls << 'http://www.taiyosha.co.jp/comic/comic' + m + '_date2.html'
     end
     urls
   end
 
-  def get_months
-    (0..2).map{|i| Time.now.months_since(i).strftime('%y%m')}
+  def get_mechanize_res
+    agent = Mechanize.new
+    agent.user_agent_alias = 'Mac Safari'
+    get_urls.map do |url|
+      begin
+        agent.get url
+      rescue Mechanize::ResponseCodeError
+      end
+    end
   end
 
-  def parse_to_books(html)
+  def make_to_books(mechanize_res)
     books = []
-    doc = Hpricot html
-    (doc/'tr').each do |tr|
-      if( (tr/'td').length != 6 )
-        next
+    mechanize_res.each do |res|
+      if(res.nil?) then next end
+      res.search('tr').each do |tr|
+        tds = tr.search('td')
+        if(tds.length != 6) then next end
+        release = tds[0].text.slice(0,5)
+        publisher = tds[1].text
+        title = tds[2].text
+        author = tds[3].text
+        books << Book.new(:title=>title, :author=>author, :publisher=>publisher, :release=>release)
       end
-      book_attr = []
-      (tr/'td').each_with_index do |td, index|
-        content = td.inner_html
-        if (index == 0)
-          content = content.gsub(/<a.*a>/, '').tr('&nbsp;', '')
-        end
-        book_attr << content
-      end
-      books << Book.new(:release=>book_attr[0],:publisher=>book_attr[1],
-                   :title=>book_attr[2],:author=>book_attr[3])
     end
     books
   end
