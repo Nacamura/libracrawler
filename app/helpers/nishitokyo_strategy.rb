bb700881398452e8e2b9dcb435f493acff61d396
Nacamura <t.nacamura@gmail.com>
2014-03-17 16:56:01 +0900

    show link in nishitokyo notifier

--- app/helpers/nishitokyo_strategy.rb
+++ app/helpers/nishitokyo_strategy.rb
@@ -36,7 +36,8 @@ module NishitokyoStrategy
       author = dds[2].text
       publisher = dds[-1].text
       title = img.attribute("alt").value
-      books << Book.new(:title=>title, :author=>author, :publisher=>publisher, :year=>year)
+      release = "http://www.library.city.nishitokyo.lg.jp" + (tr.search("a").attribute("href").value).tr('.', '')
+      books << Book.new(:title=>title, :author=>author, :publisher=>publisher, :year=>year, :release=>release)
     end
     books
   end
