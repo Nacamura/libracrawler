c69b74103bcc2671603d5a6cb593f296ffcb4364
Nacamura <t.nacamura@gmail.com>
2014-03-17 16:11:55 +0900

    fix bug for hit_count_alert

--- app/helpers/nishitokyo_strategy.rb
+++ app/helpers/nishitokyo_strategy.rb
@@ -1,10 +1,10 @@
 module NishitokyoStrategy
   include LogInterceptor
 
-  NO_LINK = "現在利用できません。リクエストについては図書館に直接ご相談ください。"
-
   def crawl
-    make_books(get_mechanize_res)
+    res = get_mechanize_res
+    alert_hit_count(res)
+    make_books(res)
   end
 
   def get_mechanize_res
@@ -25,15 +25,11 @@ module NishitokyoStrategy
   end
 
   def make_books(mechanize_res)
-    hit_count = mechanize_res.search("p")[2].search("span")[1].text.gsub(/件中.*/, "").to_i
-    if(hit_count > 100)
-      AlartMailer.alart_mail(self.to_s + " 検索件数が#{hit_count}件です。100件目以降は通知されません。").deliver
-    end
     books = []
     mechanize_res.search("tr").each do |tr|
       dds = tr.search("dd")
       if(dds.length < 3) then next end
-      if(dds[-1].text == NO_LINK) then next end
+      if(dds[-1].text == "現在利用できません。リクエストについては図書館に直接ご相談ください。") then next end
       img = tr.search("img")
       if(img.length == 0) then next end
       year = dds[1].text
@@ -43,6 +39,15 @@ module NishitokyoStrategy
       books << Book.new(:title=>title, :author=>author, :publisher=>publisher, :year=>year)
     end
     books
+  end
+
+  def alert_hit_count(mechanize_res)
+    hit_count = 0
+    count_xml = mechanize_res.search("p")[2].search("span")[1]
+    if(count_xml) then hit_count = count_xml.text.gsub(/件中.*/, "").to_i end
+    if(hit_count > 100)
+      AlartMailer.alart_mail(self.to_s + " 検索件数が#{hit_count}件です。100件目以降は通知されません。").deliver
+    end
   end
 
 end
